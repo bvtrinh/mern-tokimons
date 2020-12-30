281196b89d46d75cad9da8509e5a599cae71c468
Tyler Trinh <tylervtrinh@gmail.com>
2020-12-30 13:42:52 -0800

    Integrate bootstrap with react

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
-import "./App.css";
+import Container from "react-bootstrap/Container";
+import Jumbotron from "react-bootstrap/Jumbotron";
 
 function App() {
-  return <div className="App">I'm not saying Hello World here</div>;
+  return (
+    <div className="App">
+      <Container>
+        <Jumbotron>
+          <h1>React with Bootstrap and TypeScript</h1>
+          <p>I'm not saying Hello World here</p>
+        </Jumbotron>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
