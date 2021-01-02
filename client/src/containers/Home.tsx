ba86a81c2f390cb45a04514fb8a9af1d4d1f8eae
Tyler Trinh <tylervtrinh@gmail.com>
2021-01-01 21:01:46 -0800

    Add completed home page

--- client/src/containers/Home.tsx
+++ client/src/containers/Home.tsx
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
-// import classes from "./Home.module.css";
+import Container from "react-bootstrap/Container";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Image from "react-bootstrap/Image";
+import InputGroup from "react-bootstrap/InputGroup";
+import Button from "react-bootstrap/Button";
+import FormControl from "react-bootstrap/FormControl";
+import { FcSearch } from "react-icons/fc";
+
+import classes from "./Home.module.css";
 
 class Home extends Component {
   state = {
@@ -7,7 +15,29 @@ class Home extends Component {
   };
 
   render() {
-    return <h1>Home</h1>;
+    const search = this.state.loggedIn ? (
+      <InputGroup className="mb-3">
+        <FormControl
+          placeholder="Search for your Tokimons!"
+          aria-label="tokimon-search"
+        />
+        <InputGroup.Append>
+          <Button variant="outline-primary">
+            <FcSearch />
+          </Button>
+        </InputGroup.Append>
+      </InputGroup>
+    ) : null;
+    return (
+      <Container>
+        <Jumbotron className={classes.Jumbotron}>
+          <Image fluid src="/images/full-tokimon.png" alt="jumbo-logo" />
+          <hr />
+          <h3>Welcome to the world of Tokimons!</h3>
+        </Jumbotron>
+        {search}
+      </Container>
+    );
   }
 }
 
