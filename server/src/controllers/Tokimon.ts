29e3d01d505e287aa0348af1f82250f1a20095fa
Tyler Trinh <tylervtrinh@gmail.com>
2021-01-03 11:42:25 -0800

    Fix update op and add delete op

--- server/src/controllers/Tokimon.ts
+++ server/src/controllers/Tokimon.ts
@@ -14,13 +14,14 @@ const getHighestType = (elements: IElement) => {
     }
     total += val;
   }
+  console.log(total);
+
   return { type: bestType, total: total };
 };
 
 export const createToki: RequestHandler = async (req, res) => {
   // Extract information from request
   const tokiData = req.body;
-  const elements = tokiData.elements;
 
   // Validate data here
   const errors = validationResult(req);
@@ -28,7 +29,7 @@ export const createToki: RequestHandler = async (req, res) => {
     return res.status(422).json({ errors: errors.array() });
   }
 
-  const { type, total } = getHighestType(elements);
+  const { type, total } = getHighestType(tokiData.elements);
 
   const newToki: ITokimon = new Tokimon({
     ...tokiData,
@@ -66,12 +67,16 @@ export const getAllToki: RequestHandler = async (req, res) => {
 
 export const updateToki: RequestHandler = async (req, res) => {
   // Extract information from URL
-  const id = req.params._id;
+  const id = req.params.id;
   const newToki: ITokimon = req.body;
 
   // Find the Tokimon in DB
   try {
-    await Tokimon.findById(id);
+    const oldToki = await Tokimon.findById(id);
+    if (!oldToki) {
+      // ID does not exist
+      throw new Error("ID does not exist");
+    }
 
     // Validate the new fields
     const errors = validationResult(req);
@@ -80,22 +85,33 @@ export const updateToki: RequestHandler = async (req, res) => {
     }
 
     // Update in case the Tokimon has a new higher value to determine its type
-    const elements = newToki.elements;
-    const { type, total } = getHighestType(elements);
+    const { type, total } = getHighestType(newToki.elements);
     newToki.type = type;
     newToki.total = total;
 
     // Update on DB
-    await Tokimon.updateOne({ id: id }, newToki);
+    await Tokimon.updateOne({ _id: id }, newToki);
 
     return res
-      .status(201)
+      .status(200)
       .json({ message: "Updated the Tokimon.", updatedToki: newToki });
   } catch (err) {
     // If it hits here most likely unable to find Tokimon
     console.log(err);
-    return res.status(500).json({ error: err });
+    return res.status(500).json({ error: err.message });
   }
 };
 
-export const deleteToki: RequestHandler = async (req, res) => {};
+export const deleteToki: RequestHandler = async (req, res) => {
+  // Extract information from URL
+  const id = req.params.id;
+
+  // Delete the Tokimon in DB
+  try {
+    await Tokimon.deleteOne({ _id: id });
+    return res.status(200).json({ message: "Deleted the Tokimon." });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: err.message });
+  }
+};
