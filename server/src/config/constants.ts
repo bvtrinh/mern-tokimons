91a21f956e79c0feffc874494e2c97a2890ca4e1
Tyler Trinh <tylervtrinh@gmail.com>
2021-01-06 00:04:08 -0800

    refactor: set max len of strings in forms lower

--- server/src/config/constants.ts
+++ server/src/config/constants.ts
@@ -4,7 +4,7 @@ export const MIN_LEVEL = 1;
 export const MAX_LEVEL = 100;
 
 export const MIN_LEN = 2;
-export const MAX_LEN = 128;
+export const MAX_LEN = 32;
 
-export const MIN_LEN_PASS = 6;
-export const MAX_LEN_PASS = 128;
+export const MIN_LEN_PASS = 8;
+export const MAX_LEN_PASS = 20;
